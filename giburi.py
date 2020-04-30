b94da7a74144aba02e21c9aa9202a190af469b31
FUMI-POPPIN <fumitake.kawasaki@el-soul.com>
2020-05-01 06:54:50 +0900

    add json parse

--- giburi.py
+++ giburi.py
@@ -1,5 +1,8 @@
 import requests
+import json
 
 response = requests.get('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
 print(response.status_code)
-print(response.text)
+json_data = response.text
+title = json.loads(json_data)['title']
+print(title)
