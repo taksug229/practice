849c175caf9e1109cd0f7c6d705874e143e18629
Takeshi Sugiyama <taksug229@gmail.com>
2020-04-30 15:05:24 -0700

    Takeshi commits

--- giburi.py
+++ giburi.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import pymongo import MongoClient
+from pymongo import MongoClient
 import pandas as pd
 
 response = requests.get('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49')
